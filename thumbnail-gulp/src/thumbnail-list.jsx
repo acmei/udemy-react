ae2ae265921beb916cb18fa5599d7943a2234d7e
Anita <asovay@gmail.com>
2015-11-06 14:02:21 -0800

    added requirements to each src file

--- thumbnail-gulp/src/thumbnail-list.jsx
+++ thumbnail-gulp/src/thumbnail-list.jsx
@@ -1,8 +1,12 @@
-var ThumbnailList = React.createClass({
+var React = require('react');
+var Thumbnail = require('./thumbnail');
+
+module.exports = React.createClass({
   render: function() {
     var list = this.props.thumbnailData.map(function(thumbnailProps) {
       return <Thumbnail {...thumbnailProps}/>
     });
+    
     return <div>
       {list}
     </div>
