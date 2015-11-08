a82ac6d69eb3cf9f3037ec8fcf39bf7c9d7ca606
Anita <asovay@gmail.com>
2015-11-08 11:41:16 -0800

    WIP list item component and display toggle

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,20 +4,27 @@
 
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
     alert('hello from dropdown');
   },
   render: function() {
-
+    var list = this.props.items.map(function(item) {
+      return <ListItem item={item} />
+    });
+    
     return <div className='dropdown'>
       <Button 
       whenClicked={this.handleClick} 
       className='btn-default' 
       title={this.props.title} 
-      subTitleClassName='caret' />
+      subTitleClassName='caret' 
+      />
+      <ul>
+        {list}
+      </ul>
     </div>
   }
 })
